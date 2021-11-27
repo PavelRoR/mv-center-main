f2f65a8ffa134f584d07398b4651b192fcecf0b1
PavelRoR <pavelvp2182@yandex.ru>
2021-11-27 16:38:04 +0500

    Main page is typeseted

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#about']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
 
-
-
-/*Конец документа*/
+    $('.can-acc-item-button').on('click', function () {
+        if (!$(this).next().hasClass('cii-active')) {
+            $('.can-acc-item-button').next().removeClass('cii-active').slideUp(500);
+            $(this).next().addClass('cii-active').slideDown(500);
+        } else {
+            $(this).next().removeClass('cii-active').slideUp(500);
+        }
+    });
+    /*Конец документа*/
 });
